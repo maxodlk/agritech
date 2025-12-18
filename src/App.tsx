bb490c9269b722cbac72a5658b7a556ff20f8501
maxodlk <maximedeleke8@gmail.com>
2025-12-19 00:03:16 +0100

    so,ethngm

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import CultureImg from "./culture.jpg"
 import { Sprout, TrendingUp, Users, Award, Calculator, Menu, X } from 'lucide-react';
 
 function App() {
@@ -199,7 +200,7 @@ function App() {
               <div className="relative">
                 <div className="backdrop-blur-xl bg-white/5 rounded-3xl border border-white/10 p-4 overflow-hidden">
                   <img
-                    src="src/culture.jpg"
+                    src={CultureImg}
                     alt="Agriculture moderne au Bénin"
                     className="w-full h-[500px] object-cover rounded-2xl"
                   />
